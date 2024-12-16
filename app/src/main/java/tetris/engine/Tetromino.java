c70b2a9b326f87a440aef5d5e0f35eb877888987
zsun-ubuntu <nakani2623@outlook.com>
2024-12-17 02:53:43 +1100

    Tetromino policy: moves and rotates

--- app/src/main/java/tetris/engine/Tetromino.java
+++ app/src/main/java/tetris/engine/Tetromino.java
@@ -4,10 +4,18 @@ import java.util.List;
 
 import javafx.geometry.Point2D;
 
-/*
+enum Direction {
+    LEFT, RIGHT, UP, DOWN;
+}
+
+enum Rotation {
+    CLOCKWISE, CUNTER_CLOCKWISE, R_180;
+}
+
+/**
  * Polyomino that consists of 4 Minoes 
  */
-public class Tetromino {
+abstract public class Tetromino {
     final int order = 4;
     Point2D centre;
     List<Mino> components;
@@ -16,4 +24,17 @@ public class Tetromino {
     public Tetromino() {
         children = new Mino[order];
     }
+
+    /**
+     * Moves the tetromino a number of unit spaces
+     * @param direction
+     * @param distance
+     */
+    abstract public void move(int distance, Direction direction);
+
+    /**
+     * Rotates the tetromino
+     * @param rotation
+     */
+    abstract public void rotate(Rotation rotation);
 }
