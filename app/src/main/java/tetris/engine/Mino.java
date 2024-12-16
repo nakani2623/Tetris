fde40d62034a8ef982dcc56cf797c10c98da9d47
zsun-ubuntu <nakani2623@outlook.com>
2024-12-16 21:32:56 +1100

    tetromino data structures created

---
+++ app/src/main/java/tetris/engine/Mino.java
@@ -0,0 +1,10 @@
+package tetris.engine;
+
+import javafx.geometry.Point2D;
+
+/*
+ * square component of Polyominoes
+ */
+public class Mino {
+    Point2D position;
+}
---
+++ app/src/main/java/tetris/engine/Tetromino.java
@@ -0,0 +1,14 @@
+package tetris.engine;
+
+import java.util.List;
+
+import javafx.geometry.Point2D;
+
+/*
+ * Polyomino that consists of 4 Minoes 
+ */
+public class Tetromino {
+    final int order = 4;
+    Point2D centre;
+    List<Mino> components;
+}
