9400ea36b6b231b34b6bdef8819ab42760b5cde9
zsun-ubuntu <nakani2623@outlook.com>
2024-12-17 02:07:50 +1100

    Board ds policy change: added a List of ref to all Tetrominoes

--- app/src/main/java/tetris/engine/Board.java
+++ app/src/main/java/tetris/engine/Board.java
@@ -1,9 +1,11 @@
 package tetris.engine;
 
+import java.util.List;
 import javafx.geometry.Dimension2D;
 
 public class Board {
     private Dimension2D size;
+    List<Tetromino> allTetromino;
 
     public Board(double width, double height) {
         this.size = new Dimension2D(width, height);
