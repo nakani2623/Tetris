d5604ee3c7712dc7e458704682ef0c429b13ab3d
zsun-ubuntu <nakani2623@outlook.com>
2024-12-18 05:37:58 +1100

    added defualt constructor for Board

--- app/src/main/java/tetris/engine/Board.java
+++ app/src/main/java/tetris/engine/Board.java
@@ -7,8 +7,20 @@ public class Board {
     private Dimension2D size;
     List<Mino> allMinos;
 
+    /**
+     * Constructs game bord with specific dimension
+     * @param width
+     * @param height
+     */
     public Board(double width, double height) {
         this.size = new Dimension2D(width, height);
+    }
+
+    /**
+     * Constructs default game board 10x20 dimension
+     */
+    public Board() {
+        this.size = new Dimension2D(10, 20);
     }
 
     public double getWidth() {
