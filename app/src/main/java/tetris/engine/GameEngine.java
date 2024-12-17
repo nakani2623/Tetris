b39bb9f513d800ae8a95c661be51620ebc1aeedc
zsun-ubuntu <nakani2623@outlook.com>
2024-12-18 05:39:29 +1100

    created GameEngine constructor: alloc's game board

--- app/src/main/java/tetris/engine/GameEngine.java
+++ app/src/main/java/tetris/engine/GameEngine.java
@@ -3,6 +3,10 @@ package tetris.engine;
 public class GameEngine {
     private Board board;
 
+    public GameEngine() {
+        board = new Board();
+    }
+
     public Board getBoard() {
         return board;
     }
