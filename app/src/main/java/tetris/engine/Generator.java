2acba7618ddd60d536db6203c0bbb8cb47434634
zsun2623 <zsun2623@uni.sydney.edu.au>
2025-05-09 03:54:03 +1000

    tetromino generator algo scratch

---
+++ app/src/main/java/tetris/engine/Generator.java
@@ -0,0 +1,49 @@
+package tetris.engine;
+
+import javafx.geometry.Point2D;
+import tetris.engine.type.TetrominoType;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Tetromino generator
+ */
+public class Generator {
+
+//    public Tetromino run() {
+//        return i();
+//    }
+
+    /**
+     * Generates 1 bag of Tetrominos
+     * @return A List of 7 Tetrominos
+     */
+    // public List<Tetromino> bag7() {
+    //     //TODO: impl
+    //     List<Tetromino> tetrominos = new ArrayList<>();
+    //     for (TetrominoType type : TetrominoType.values()) {
+    //         Tetromino t = new Tetromino(new Point2D(5, 10), type);
+    //         tetrominos.add(t);
+    //     }
+
+    //     Collections.shuffle(tetrominos);
+    //     return tetrominos;
+    // }
+
+//    public Tetromino i() {
+//        Tetromino tetromino = new Tetromino(null);
+//        tetromino.type = TetrominoType.I;
+//        return tetromino;
+//    }
+//
+//    public Tetromino t() {
+//
+//        Tetromino tetromino = new Tetromino(null);
+//        tetromino.type = TetrominoType.T;
+//        return tetromino;
+//    }
+
+
+}
